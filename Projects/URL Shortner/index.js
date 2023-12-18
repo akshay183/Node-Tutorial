493a503bd1eef44be7ec68519f80a0e009f93d50
183akshay <sharmahoney398@gmail.com>
2023-12-18 23:41:53 +0530

    Project-URL_Shortener-basics of EJS learned. check to see basics tag working, variable passing and all.

--- Projects/URL Shortner/index.js
+++ Projects/URL Shortner/index.js
@@ -1,16 +1,34 @@
 const express = require('express')
+const path = require('path');
 const app = express()
-const PORT = 3000
+const User = require('./models/urlModel.js')
+const PORT = 8000
 const router = require('./routes/urlRouter')
 const {mongoDBConnect} = require('./connection.js')
 
 app.use(express.json())
+
+app.set('view engine', 'ejs');
+app.set('views', path.resolve('./views'))
 
 // mongoDB connect
 mongoDBConnect("mongodb://127.0.0.1:27017/tutorial-db").
     then(() => {
         console.log("mongoose connected");
     })
+
+// request
+app.get('/test', async (req, res) => {
+    
+    const urls = await User.find({})
+    return res.render('home', {urls})
+})
+
+app.get('/test/new_doc', async (req, res) => {
+    
+    const urls = await User.find({})
+    return res.render('home')
+})
 
 // route
 app.use('/api', router)
